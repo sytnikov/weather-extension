6ddb3485314c41088e2033087088fe91a4b6eb77
sytnikov <sytnikov.alexey@gmail.com>
2024-08-12 19:39:27 +0300

    storage functionality added

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { Grid, Box, Paper, InputBase, IconButton } from '@mui/material'
@@ -7,22 +7,33 @@ import AddIcon from '@mui/icons-material/Add'
 import 'fontsource-roboto'
 import './popup.css'
 import WeatherCard from './WeatherCard'
+import { getStoredCities, setStoredCities } from '../utils/storage'
 
 const App: React.FC<{}> = () => {
-  const [cities, setCities] = useState<string[]>(['Helsinki', 'Turku', 'Error'])
+  const [cities, setCities] = useState<string[]>([])
   const [cityInput, setCityInput] = useState<string>('')
+
+  useEffect(() => {
+    getStoredCities().then((cities) => setCities(cities))
+  }, [])
 
   const handleAddCityButtonClick = () => {
     if (cityInput === '') {
       return
     }
-    setCities([...cities, cityInput])
-    setCityInput('')
+    const updatedCities = [...cities, cityInput]
+    setStoredCities(updatedCities).then(() => {
+      setCities(updatedCities)
+      setCityInput('')
+    })
   }
 
   const handleDeleteCityButtonClick = (index: number) => {
     cities.splice(index, 1)
-    setCities([...cities])
+    const updatedCities = [...cities]
+    setStoredCities(updatedCities).then(() => {
+      setCities(updatedCities)
+    })
   }
 
   return (
@@ -34,13 +45,14 @@ const App: React.FC<{}> = () => {
           // justifyContent="space-between"
           // alignItems="center"
         >
-          <Grid item>
+          <Grid item xs={12}>
             <Paper>
-              <Box px={'15px'} py={'5px'}>
+              <Box px={'15px'} py={'5px'} display={'flex'}>
                 <InputBase
                   placeholder="Add a city name"
                   value={cityInput}
                   onChange={(event) => setCityInput(event.target.value)}
+                  fullWidth
                 />
                 <IconButton onClick={handleAddCityButtonClick}>
                   <AddIcon />
@@ -50,7 +62,11 @@ const App: React.FC<{}> = () => {
           </Grid>
         </Grid>
         {cities.map((city, index) => (
-          <WeatherCard city={city} key={index} onDelete={() => handleDeleteCityButtonClick(index)}/>
+          <WeatherCard
+            city={city}
+            key={index}
+            onDelete={() => handleDeleteCityButtonClick(index)}
+          />
         ))}
       </Box>
       <Box height={'16px'} />
