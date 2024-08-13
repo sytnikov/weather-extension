4bd55d43c0586ebb9028c1319a3a4727d7cf2e92
sytnikov <sytnikov.alexey@gmail.com>
2024-08-13 21:36:16 +0300

    temperature badge feature added

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,10 @@
-import { getStoredCities, setStoredCities, setStoredOptions } from '../utils/storage'
+import { fetchOpenWeatherData } from '../utils/api'
+import {
+  getStoredCities,
+  getStoredOptions,
+  setStoredCities,
+  setStoredOptions,
+} from '../utils/storage'
 
 chrome.runtime.onInstalled.addListener(() => {
   setStoredCities([])
@@ -13,10 +19,29 @@ chrome.runtime.onInstalled.addListener(() => {
     title: 'Add city to weather extension',
     id: 'weatherExtension',
   })
+
+  chrome.alarms.create({
+    periodInMinutes: 1 / 12,
+  })
 })
 
 chrome.contextMenus.onClicked.addListener((event) => {
   getStoredCities().then((cities) => {
     setStoredCities([...cities, event.selectionText])
+  })
+})
+
+chrome.alarms.onAlarm.addListener(() => {
+  getStoredOptions().then((options) => {
+    if (options.homeCity === '') {
+      return
+    }
+    fetchOpenWeatherData(options.homeCity, options.tempScale).then((data) => {
+      const temp = Math.round(data.main.temp)
+      const symbol = options.tempScale === 'metric' ? '\u2103' : '\u2109'
+      chrome.action.setBadgeText({
+        text: `${temp}${symbol}`,
+      })
+    })
   })
 })
