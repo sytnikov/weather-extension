47b19e1dfac2620a59527acb2696ed347ec24bd9
sytnikov <sytnikov.alexey@gmail.com>
2024-08-13 21:17:18 +0300

    add city through a context menu

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,10 +1,22 @@
-import { setStoredCities, setStoredOptions } from '../utils/storage'
+import { getStoredCities, setStoredCities, setStoredOptions } from '../utils/storage'
 
 chrome.runtime.onInstalled.addListener(() => {
-  setStoredCities([]),
-    setStoredOptions({
-      hasAutoOverlay: false,
-      homeCity: '',
-      tempScale: 'metric',
-    })
+  setStoredCities([])
+  setStoredOptions({
+    hasAutoOverlay: false,
+    homeCity: '',
+    tempScale: 'metric',
+  })
+
+  chrome.contextMenus.create({
+    contexts: ['selection'],
+    title: 'Add city to weather extension',
+    id: 'weatherExtension',
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((event) => {
+  getStoredCities().then((cities) => {
+    setStoredCities([...cities, event.selectionText])
+  })
 })
